90a2acf31daacac2df9a05fba0523ca7496d729e
sandeep524 <36640058+sandeep524@users.noreply.github.com>
2018-08-16 19:21:33 +0530

    Update ansearch.php

--- ansearch.php
+++ ansearch.php
@@ -12,10 +12,7 @@
  <table cellpadding="20" cellspacing="20" width="1000px" height="200px"  style="margin:auto" >
   
  <tr  align="center" class="bold">            
-			
-
-			
-            
+ 
            <th  style="color:red"align="center">NAME</th>
 			
             <th   style="color:red" align="center">AGE</th>
@@ -25,25 +22,17 @@
             <th  style="color:red" align="center">MOBILE</th>
             
             <th  style="color:red" align="center">EMAIL</th>
-            
-
-            
+                
             <th  style="color:red" align="center">CITY</th>
             
             <th  style="color:red" align="center">GENDER</th>
-            
-            
+                  
             <th  style="color:red" align="center">LAST DONATED ON</th>
         </tr>
-                   
-
-
-
+            
 <?php
 
-	
 $cn=mysqli_connect("localhost","root","","donors");
-
 $date=date("y-m-d");
 $sub=date('Y-m-d',(strtotime ( '-90 day' , strtotime ( $date) ) ));
 echo("minimum donation date is below:" );print_r($sub);
@@ -56,13 +45,11 @@ $s="SELECT * FROM reg WHERE bg=\"A-\" AND DD<\"$sub\" and age>\"18\" order by DD
 	while($data=mysqli_fetch_array($result))
 	{
 				echo"<tr  align='center' >
-
 				<td style='color:steelblue' style=' padding-left:20px'>$data[1]</td>
 				<td style='color:steelblue' style=' padding-left:50px'>$data[2]</td>
 				<td style='color:steelblue' style=' padding-left:50px'>$data[3]</td>
 				<td style='color:steelblue' style=' padding-left:60px'>$data[4]</td>
 				<td style='color:steelblue' style=' padding-left:60px'>$data[5]</td>
-
 				<td style='color:steelblue' style=' padding-left:0px'>$data[7]</td>
 				<td style='color:steelblue' style=' padding-left:60px'>$data[8]</td>
 				<td style='color:steelblue' style=' padding-left:60px'>$data[9]</td>";
